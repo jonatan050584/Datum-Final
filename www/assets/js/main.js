ac9c8d25c65a6ecd6e9abd6fd5e0f61f25503118
jonatan050584 <jonatan_angelo@hotmail.com>
2015-08-27 19:13:43 -0500

    12

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,6 +1,21 @@
+var nav = new Array("home");
+
 $(document).ready(function(){
+	$("#nav .back").on("tap",function(){
+		$("#"+nav.pop()).hide();
+		$("#"+nav[nav.length-1]).show();
+		if(nav.length==1) $(this).hide();
+	})
+
 	$("#home .btempezar").on("tap",function(){
 		$("#home").hide();
 		$("#menu").show();
-	})
+		$("#nav .back").show();
+		nav.push("menu");
+	});
+	$("#menu .item.cat").on("tap",function(){
+		$("#menu").hide();
+		$("#categoria").show();
+		nav.push("categoria");
+	});
 })
