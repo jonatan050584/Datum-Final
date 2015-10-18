d48de437871d3c15421ade8dbe46adef025546fb
jonatan050584 <jonatan_angelo@hotmail.com>
2015-10-17 20:45:42 -0500

    13

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,6 +1,25 @@
 var nav = new Array("home");
+var path = "";
+var production=1;
+
+var htmlItem = '<div class="item cat" id="cat-{id}">'+
+		'<div class="bt">'+
+			'<div class="nom"><div class="txt">{nombre}</div></div>'+
+		'</div>'+
+	'</div>';
+
+var data;
 
 $(document).ready(function(){
+
+	if(production==1) 
+		path="http://picnic.pe/clientes/datum/app/";
+	else
+		path="http://localhost/datum/";
+
+	ini_datos();
+	
+
 	$("#nav .back").on("tap",function(){
 		$("#"+nav.pop()).hide();
 		$("#"+nav[nav.length-1]).show();
@@ -19,3 +38,32 @@ $(document).ready(function(){
 		nav.push("categoria");
 	});
 })
+function ini_datos(){
+	$.ajax({
+		url:ruta("api/data.json"),
+		dataType:'json'
+	}).done(function(res){
+		data = res.data;
+		cargar_categorias();
+	});
+}
+
+function cargar_categorias(){
+
+	var div = $(".seccion#menu");
+
+	var preHTML = div.html();
+	div.empty();
+	$.each(data,function(key,val){
+		var html = htmlItem;
+			html = html.replace("{nombre}",val.nombre);
+			html = html.replace("{id}",val.id);
+		div.append(html);
+		$("#cat-"+val.id+" .bt").css("background-image","url("+ruta("files/"+val.icono)+")");
+	});
+	div.append(preHTML);
+}
+
+function ruta(str){
+	return path+str;
+}
